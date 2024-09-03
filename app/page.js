2630d71cd4437e4efd01f4f8788302693f9be813
Marcelo Cesar <jsdevmarcelo@gmail.com>
2024-09-03 15:34:15 -0100

    google script wrap up inside Head element

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import {Box, Stack, TextField, Button, Typography} from '@mui/material';
 import ReactMarkdown from 'react-markdown';
 // useRouter
 import { useRouter } from 'next/navigation'
+import Head from 'next/head';
 
 function useIsMobile() {
   const [isMobile, setIsMobile] = useState(true);
@@ -93,190 +94,195 @@ export default function Home() {
   }
   
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-    >
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GTAG_API_KEY}`}/>
-      <script dangerouslySetInnerHTML={
-        {
-          __html: `  
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+    <>
+      <Head>
+        <title></title>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GTAG_API_KEY}`}/>
+        <script dangerouslySetInnerHTML={
+          {
+            __html: `  
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GTAG_API_KEY}');
-          `
-        }
-      } />
+            gtag('config', '${process.env.NEXT_PUBLIC_GTAG_API_KEY}');
+            `
+          }
+        } />
+      </Head>
       <Box
         width="100vw"
-        height="10vh"
-        bgcolor="black"
-        display="flex"
-        alignItems="center"
-      >
-        <Typography
-          variant={isMobile ? "h5" : "h3"}
-          marginLeft={4}
-          color="white"
-          display="flex"
-          flex={3}
-        >Implement AI</Typography>
-       <Box
-          marginRight={4}
-          gap={2}
-       >
-        <Button
-          variant="outlined"
-          onClick={() => {
-            // canShowChat()
-            router.push("/inquiry_now")
-            // send to another page and fill out the blanks then it suposes to send an email
-          }}
-        >
-        Inquiry Now
-        </Button>
-       </Box>
-      </Box>
-      <Box
-        height="90vh"
+        height="100vh"
         display="flex"
         flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        bgcolor="black"
       >
         <Box
-          display={canTry ? "none" : "flex"}
           width="100vw"
+          height="10vh"
+          bgcolor="black"
+          display="flex"
+          alignItems="center"
+        >
+          <Typography
+            variant={isMobile ? "h5" : "h3"}
+            marginLeft={4}
+            color="white"
+            display="flex"
+            flex={3}
+          >Implement AI</Typography>
+        <Box
+            marginRight={4}
+            gap={2}
+        >
+          <Button
+            variant="outlined"
+            onClick={() => {
+              // canShowChat()
+              router.push("/inquiry_now")
+              // send to another page and fill out the blanks then it suposes to send an email
+            }}
+          >
+          Inquiry Now
+          </Button>
+        </Box>
+        </Box>
+        <Box
           height="90vh"
-          justifyContent="center"
+          display="flex"
           flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="black"
         >
           <Box
-            flex={1}
-            alignContent="center"
+            display={canTry ? "none" : "flex"}
+            width="100vw"
+            height="90vh"
+            justifyContent="center"
+            flexDirection="column"
           >
-              <Box
-                width="80vw"
-                marginLeft={4}
-              >
-                <Typography
-                  variant={isMobile ? "h5" : "h2"}
-                  color="white"
+            <Box
+              flex={1}
+              alignContent="center"
+            >
+                <Box
+                  width="80vw"
+                  marginLeft={4}
                 >
-                  Implement AI Chat Assistant specialized for your business within minutes.
-                </Typography>
+                  <Typography
+                    variant={isMobile ? "h5" : "h2"}
+                    color="white"
+                  >
+                    Implement AI Chat Assistant specialized for your business within minutes.
+                  </Typography>
+                </Box>
+                <Box
+                  display="flex"
+                  width="80vw"
+                >
+                  <Box
+                    marginLeft={4}
+                    marginTop={4}
+                  >
+                    <Button variant="contained" onClick={() => {
+                      canShowChat();
+                    }}>Try it out</Button>
+                  </Box>
+                </Box>
               </Box>
               <Box
                 display="flex"
-                width="80vw"
+                justifyContent="center"
+                alignItems="center"
+                textAlign="center"
               >
-                <Box
-                  marginLeft={4}
-                  marginTop={4}
-                >
-                  <Button variant="contained" onClick={() => {
-                    canShowChat();
-                  }}>Try it out</Button>
-                </Box>
+                <Typography variant="h7" color="white">Copyright © 2024 Implement AI. All rights reserved.</Typography>
               </Box>
             </Box>
+          <Box
+              display={canTry ? "flex" : "none"}
+          >
+            <Stack
+                direction="column"
+                width={isMobile ? "90vw" : "50vw"}
+                height="80vh"
+                border="1px solid white"
+                borderRadius={2}
+                p={2}
+                spacing={2}
+            >
+              <Stack
+                direction="column"
+                flexGrow={1}
+                spacing={2}
+                overflow="auto"
+                maxHeight="100%"
+              >
+                {
+                  messages.map((message, index)=>(
+                    <Box 
+                      key={index}
+                      display="flex"
+                      justifyContent={
+                        message.role === 'assistant' ? 'flex-start' : 'flex-end'
+                      }
+                    >
+                      <Box
+                        bgcolor={
+                          message.role === 'assistant' ? 'primary.main' : 'secondary.main' 
+                        }
+                        color="white"
+                        p={message.content.includes('*') ? 4 : 2}
+                        borderRadius={2}
+                        width={message.content.length > 20 ? "70%" : "40%"}
+                      >
+                        <ReactMarkdown>{message.content}</ReactMarkdown>
+                      </Box>
+                    </Box>
+                  ))
+                }
+              </Stack>
+              <Stack
+                direction="row"
+                spacing={2}
+                display={limit >= 6 ? "none" : "flex"}
+              >
+                <TextField
+                  value={message}
+                  onChange={(e)=> {setMessage(e.target.value)}}
+                  fullWidth
+                  label="message"
+                  variant="outlined"
+                  focused
+                  sx={{
+                    input: {
+                      color: 'white',
+                    },
+                  }}
+                />
+                <Button variant="contained" onClick={sendMessage}>Send</Button>
+              </Stack>
             <Box
-              display="flex"
+              display={limit >= 6 ? "flex" : "none"}
               justifyContent="center"
               alignItems="center"
-              textAlign="center"
+              gap={2}
             >
-              <Typography variant="h7" color="white">Copyright © 2024 Implement AI. All rights reserved.</Typography>
+                  <Typography variant="p" color="grey">Limit reached</Typography>
+                  <Button
+                      variant="outlined"
+                      onClick={() => {
+                        router.push("/inquiry_now")
+                        // send to another page and fill out the blanks then it suposes to send an email
+                      }}
+                      >
+                      Inquiry Now
+                  </Button>
             </Box>
-          </Box>
-        <Box
-            display={canTry ? "flex" : "none"}
-        >
-          <Stack
-              direction="column"
-              width={isMobile ? "90vw" : "50vw"}
-              height="80vh"
-              border="1px solid white"
-              borderRadius={2}
-              p={2}
-              spacing={2}
-          >
-            <Stack
-              direction="column"
-              flexGrow={1}
-              spacing={2}
-              overflow="auto"
-              maxHeight="100%"
-            >
-              {
-                messages.map((message, index)=>(
-                  <Box 
-                    key={index}
-                    display="flex"
-                    justifyContent={
-                      message.role === 'assistant' ? 'flex-start' : 'flex-end'
-                    }
-                  >
-                    <Box
-                      bgcolor={
-                        message.role === 'assistant' ? 'primary.main' : 'secondary.main' 
-                      }
-                      color="white"
-                      p={message.content.includes('*') ? 4 : 2}
-                      borderRadius={2}
-                      width={message.content.length > 20 ? "70%" : "40%"}
-                    >
-                      <ReactMarkdown>{message.content}</ReactMarkdown>
-                    </Box>
-                  </Box>
-                ))
-              }
             </Stack>
-            <Stack
-              direction="row"
-              spacing={2}
-              display={limit >= 6 ? "none" : "flex"}
-            >
-              <TextField
-                value={message}
-                onChange={(e)=> {setMessage(e.target.value)}}
-                fullWidth
-                label="message"
-                variant="outlined"
-                focused
-                sx={{
-                  input: {
-                    color: 'white',
-                  },
-                }}
-              />
-              <Button variant="contained" onClick={sendMessage}>Send</Button>
-            </Stack>
-          <Box
-            display={limit >= 6 ? "flex" : "none"}
-            justifyContent="center"
-            alignItems="center"
-            gap={2}
-          >
-                <Typography variant="p" color="grey">Limit reached</Typography>
-                <Button
-                    variant="outlined"
-                    onClick={() => {
-                      router.push("/inquiry_now")
-                      // send to another page and fill out the blanks then it suposes to send an email
-                    }}
-                    >
-                    Inquiry Now
-                </Button>
           </Box>
-          </Stack>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 }
